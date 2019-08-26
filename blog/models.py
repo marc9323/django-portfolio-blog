66234543ca8236c9d553ad0ab2a81a27cf0f5e09
unknown <marcholman9323@gmail.com>
2019-08-26 15:00:47 -0500

    jobs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+#title
+#publication date
+#body
+#image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# blog model create
+
+# add blog app to the settings
+
+# create a migration
+
+# migrate
+
+# add to the admin
