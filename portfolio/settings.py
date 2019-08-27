0f9191b57b1903c17edc9e3350e16294c23f7cc4
unknown <marcholman9323@gmail.com>
2019-08-26 21:51:23 -0500

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'batshit93',
+        'PASSWORD': 'xxxxx',
         'HOST': 'localhost',
         'PORT': 5432
     }
